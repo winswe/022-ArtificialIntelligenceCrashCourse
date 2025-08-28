a9cf3adab3c0dd4cdb93674190bbb102aed6fbd4
winswe <mws.mdm@gmail.com>
2025-08-28 23:14:11 +0630

    torch

--- 02-RandomForestClassifier.py
+++ 02-RandomForestClassifier.py
@@ -12,7 +12,7 @@ y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Initialize and train the classifier
-clf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf = RandomForestClassifier(n_estimators=50, random_state=42)
 clf.fit(X_train, y_train)
 
 # Predict on the test set
